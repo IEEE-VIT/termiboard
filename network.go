87e5d3ded7ad77c5bbe1da1b6181fae94cb25a1d
Arran White <arrwhidev@gmail.com>
2020-10-01 21:02:24 +0100

    Implemented PortChecker()

--- network.go
+++ network.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"net"
+)
+
 //func GetPublicIPAddress() {
 //	//Implement the function to return the public address of the pc.
 //	//Do not forget to handle errors if the pc isn't connected to a public network!
@@ -10,8 +14,24 @@ package main
 //	//Implement to function to return the local IP Address of the pc.
 //	// You will need to return a String.
 //}
-//
-//func PortChecker() {
-//	//Implement this function to take in a list of Strings (port numbers) , check if those ports are available or in use.
-//	//Return a list of String of unavailable ports from the received list.
-//}
+
+// Returns a list of unavailable ports from the received list.
+func PortChecker(ports []string) []string {
+	var unavailable []string
+	for _, port := range ports {
+		if isPortUnavailable(port) {
+			unavailable = append(unavailable, port)
+		}
+	}
+
+	return unavailable
+}
+
+func isPortUnavailable(port string) bool {
+	ln, err := net.Listen("tcp", ":"+port)
+	if ln != nil {
+		defer ln.Close()
+	}
+
+	return err != nil
+}

2b463653143471a019512a45087f0ec70e147f8d
Sourabh Choure <36518515+saviourcode@users.noreply.github.com>
2020-10-02 21:37:03 +0530

    Removed flags from pci.go

--- pci.go
+++ pci.go
@@ -5,21 +5,19 @@ import (
 )
 
 func GetPCIDevices() {
-	if *showPCIDevices {
-		ResultPrinter("PCI Bus Devices:", "")
-		pci, err := pcidb.New()
-		if err != nil {
-			StandardPrinter(ErrorRedColor, "Error getting PCI Devices Data")
-			panic(err)
-		}
+	ResultPrinter("PCI Bus Devices:", "")
+	pci, err := pcidb.New()
+	if err != nil {
+		StandardPrinter(ErrorRedColor, "Error getting PCI Devices Data")
+		panic(err)
+	}
 
-		for _, deviceClass := range pci.Classes {
-			StandardPrinter(BoldWhiteColor, "\tDevice class: [" + string(deviceClass.Name) + "] "+ string(deviceClass.ID))
-			for _, deviceSubclass := range deviceClass.Subclasses {
-				StandardPrinter(None, "\t\tDevice subclass:" + string(deviceSubclass.Name) + " "+ string(deviceSubclass.ID))
-				for _, programmingInterface := range deviceSubclass.ProgrammingInterfaces {
-					StandardPrinter(BoldGreenColor, "\t\t\tProgramming interface" + string(programmingInterface.Name) + " " + string(programmingInterface.ID))
-				}
+	for _, deviceClass := range pci.Classes {
+		StandardPrinter(BoldWhiteColor, "\tDevice class: ["+string(deviceClass.Name)+"] "+string(deviceClass.ID))
+		for _, deviceSubclass := range deviceClass.Subclasses {
+			StandardPrinter(None, "\t\tDevice subclass:"+string(deviceSubclass.Name)+" "+string(deviceSubclass.ID))
+			for _, programmingInterface := range deviceSubclass.ProgrammingInterfaces {
+				StandardPrinter(BoldGreenColor, "\t\t\tProgramming interface"+string(programmingInterface.Name)+" "+string(programmingInterface.ID))
 			}
 		}
 	}
